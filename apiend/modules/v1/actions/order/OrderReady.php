4b4e329d7b16683550f2abb8e6c15ed3d4f3c178
wskeee <wskeee@192.168.0.103>
2019-05-23 00:01:35 +0800

    完成订单初始准备接口

--- apiend/modules/v1/actions/order/OrderReady.php
+++ apiend/modules/v1/actions/order/OrderReady.php
@@ -5,9 +5,10 @@ namespace apiend\modules\v1\actions\order;
 use apiend\models\Response;
 use apiend\modules\v1\actions\BaseAction;
 use common\models\goods\Goods;
-use common\models\goods\GoodsSpecPrice;
+use common\models\goods\GoodsSpecItem;
 use common\models\UserAddress;
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * 下单准备
@@ -27,13 +28,39 @@ class OrderReady extends BaseAction
         }
 
         $address = UserAddress::findOne(['user_id' => Yii::$app->user->id, 'is_default' => 1]);
-        $gsp = GoodsSpecPrice::findOne(['goods_id' => $goods_id, 'is_del' => 0]);
+        $specs = $goods->goodsModel->goodsSpecs;
+        $gsps = $goods->getGoodsSpecPrices();
+        $gsp_key = $gsps[0]['spec_key'];
+        $gsps_items_ids = array_filter(explode('_', implode('_', ArrayHelper::getColumn($gsps, 'spec_key'))));
+        $spec_items = ArrayHelper::index(GoodsSpecItem::find()->where(['id' => $gsps_items_ids])->asArray()->all(), 'id');
+
+        //计算库存
+        $gsps_spec_key_store = ArrayHelper::map($gsps, 'spec_key', 'store_count');
+        $spec_store = [];
+        foreach ($gsps_spec_key_store as $spec_key => $store_count) {
+            $spec_ids = explode('_', $spec_key);
+            foreach ($spec_ids as $spec_id) {
+                $spec_item = &$spec_items[$spec_id];
+                if (!isset($spec_item['store_count'])) {
+                    $spec_item['store_count'] = 0;
+                }
+                $spec_item['store_count'] += $store_count;
+            }
+        }
+
+        $spec_items = ArrayHelper::index($spec_items, null, 'spec_id');
+        foreach ($specs as &$spec) {
+            $spec = $spec->toArray();
+            $spec['items'] = $spec_items[$spec['id']];
+        }
+
+
 
         return new Response(Response::CODE_COMMON_OK, null, [
             'goods' => $goods,
-            'specs' => $goods->goodsModel->goodsSpecs, //规格
-            'spec_items' => $goods->getGoodsSpecItems(), //规格项
-            'gsp' => $gsp,
+            'specs' => $specs, //规格
+            'gsps' => ArrayHelper::index($gsps, 'id'), //规格价格
+            'gsp_key' => $gsp_key, //默认规格
             'address' => $address->getDetail(),
         ]);
     }
