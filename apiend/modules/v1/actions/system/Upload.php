9495aa57f46e5f144ad24b024e56ccc8e63c1c64
wskeee <361733529@qq.com>
2019-08-23 18:04:29 +0800

    修改上传图片超时设置

--- apiend/modules/v1/actions/system/Upload.php
+++ apiend/modules/v1/actions/system/Upload.php
@@ -16,19 +16,20 @@ class Upload extends Action
 
     public function run()
     {
+        set_time_limit(60 * 10);
         $webuploaderConfig = Yii::$app->params['webuploader'];
         $key = $webuploaderConfig['savePath'];
-        
+
         $name = $_FILES['file']['name'];
         $pathinfo = pathinfo($name);
         $filename = md5($pathinfo['filename']) . '.' . $pathinfo['extension'];
         $object = "{$key}{$filename}";
-        try{
+        try {
             Aliyun::getOss()->multiuploadFile($object, $_FILES['file']['tmp_name']);
         } catch (Exception $ex) {
             return new Response(Response::CODE_COMMON_UNKNOWN, null, $ex->getMessage());
         }
-        
+
         return new Response(Response::CODE_COMMON_OK, null, ['url' => Aliyun::absolutePath($object)]);
     }
 
