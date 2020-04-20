0f35b3815802aa9f3d5be42e9d2a9c4ffdf30086
wskeee <wskeee@192.168.0.103>
2020-04-20 23:57:22 +0800

    更新小程序配置

--- apiend/config/params.php
+++ apiend/config/params.php
@@ -1,24 +1,4 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    /* api */
-    'api_secret_key' => 'hellobabo_api',
-    // 微信小程序配置 具体可参考EasyWechat
-    'wechatMiniProgramConfig' => [
-        'app_id' => 'wx44aeef59ce52fe7d',
-        'secret' => '66ae8e5ed102662e8e6be7ebc59252cb',
-        // 下面为可选项
-        // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
-        'response_type' => 'array',
-        'log' => [
-            'level' => 'debug',
-            'file' => __DIR__ . '/wechat.log',
-        ],
-    ],
-    // 微信开放平台第三方平台配置 具体可参考EasyWechat
-    'wechatOpenPlatformConfig' => [],
-    // 微信企业微信配置 具体可参考EasyWechat
-    'wechatWorkConfig' => [],
-    // 微信企业微信开放平台 具体可参考EasyWechat
-    'wechatOpenWorkConfig' => [],
 ];
