594eda3bf9db7797d77d22afbecad1adcf2a2599
wskeee <wskeee@192.168.0.103>
2020-06-28 23:47:45 +0800

    增加代理商新建指引

--- backend/modules/platform_admin/views/agency/create.php
+++ backend/modules/platform_admin/views/agency/create.php
@@ -12,6 +12,14 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="agency-create">
 
+    <div class="alert alert-danger" role="alert">
+        创建代理商流程：<br/>
+        1、新建后台账号，选择账号类型为代理商<br/>
+        2、把新建的账号加入代理商角色组<br/>
+        3、新建代理商，选择管理员为上一步创建的账号<br/>
+        4、绑定客服<br/>
+    </div>
+
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
